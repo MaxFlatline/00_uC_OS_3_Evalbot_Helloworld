ecb76ac2ab7b395d3f9cadd7eed2a0dc8c87e810
CatLine <x009x@yandex.ru>
2025-03-17 22:25:11 +0300

    Added main.c file

---
+++ Sources/Core/Src/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Flatline
+ * @date 17.03.2025
+ * @brief File containing user programm entry point
+ */
+
+#include <stdint.h>
+
+int main(void){
+
+    while(1){
+
+    }
+    return 0;
+}
